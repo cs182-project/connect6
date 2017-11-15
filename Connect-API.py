205af46d3e79216c62bad3c0e41c5961800855f6
kmotwani <kmotwani@users.noreply.github.com>
2017-11-14 23:21:15 -0500

    Connect6 Updated, Autoplay, Random and Bot Functions defined. Ready for inserting alorithms.

--- Connect-API.py
+++ Connect-API.py
@@ -2,15 +2,17 @@ from Tkinter import Tk, Button, Frame, Canvas
 from tkFont import Font
 from copy import deepcopy
 import random
+from enum import Enum
+import time
 
 class Board:
   def __init__(self,other=None):
     self.player = 'X'
     self.opponent = 'O'
     self.empty = '.'
-    self.width = 7
-    self.height = 7
-    self.connect = 6
+    self.width = 10
+    self.height = 10
+    self.connect = 4
     self.fields = {}
     for y in range(self.height):
       for x in range(self.width):
@@ -90,6 +92,12 @@ class Board:
         return winning
     return []
 
+
+class PlayerType(Enum):
+    Human = 1
+    Bot = 2
+    Random = 3
+
 class GUI:
   def __init__(self):
     self.board = Board()
@@ -102,24 +110,32 @@ class GUI:
     self.tiles = {}
     self.flag = True
     self.game = False
-    self.p1_positions, self.p2_positions = [], []
-    
-    #initiate turnCount for every time player moves. Doesn't count opponent's moves. Used in def move
+    self.inputx = []
     self.turnCount = 0
-    
+    self.auto_delay = 2
+    self.p1_positions, self.p2_positions = [], []
+    self.p1 = PlayerType(int(input('Player 1 is :\n1. Human \n2. Bot \n3. Random \n\n')))
+    self.p2 = PlayerType(int(input('\nPlayer 2 is :\n1. Bot \n2. Random \n\n'))+1)
+    self.human_flag = self.p1.name=="Human" or self.p2.name=="Human"
     for x in range(self.board.width):
-      handler = lambda x=x: self.move(x)
+      handler = lambda x=x: self.input(x)
       button = Button(self.app, command=handler, font=Font(family="Helvetica", size=14), text=x+1)
       button.grid(row=0, column=x, sticky="WE")
       self.buttons[x] = button
     self.frame.grid(row=1, column=0, columnspan=self.board.width)
-    
+    if not self.human_flag:
+      for x in range(self.board.width):
+        self.buttons[x]['state'] = 'disabled'
+
+      handler = lambda: self.autoplay()
+      self.auto = Button(self.app, command=handler, text='Next')
+      self.auto.grid(row=3, column=0, columnspan=self.board.width, sticky="WE")
     for x,y in self.board.fields:
       tile = Canvas(self.frame, width=60, height=50, bg="navy", highlightthickness=0)
       tile.grid(row=self.board.height-1-y, column=x)
       self.tiles[x,y] = tile
     handler = lambda: self.reset()
-    self.restart = Button(self.app, command=handler, text='reset')
+    self.restart = Button(self.app, command=handler, text='Reset')
     self.restart.grid(row=2, column=0, columnspan=self.board.width, sticky="WE")
     self.game = self.update()
 
@@ -128,54 +144,88 @@ class GUI:
     self.p1_positions, self.p2_positions = [], []
     self.flag = True
     self.game = False
+    self.inputx = []
     self.game = self.update()
-    #reset turn counter
     self.turnCount = 0
 
-  def move(self,x):
-      
-    #if turn count is odd, then player plays a stone and computer plays two stones
-    if not (self.turnCount % 2 == 0):
-        if self.game == False:
-          self.app.config(cursor="watch")
-          self.board = self.board.move(x,1)
+  def avail_cols(self):
+    filledCols = []
+    for x,y in self.p1_positions + self.p2_positions:
+      if y == self.board.height - 1:
+        filledCols.append(x)
+    availableCols = [x for x in range(self.board.height) if x not in filledCols]
+    return availableCols
+
+  def random_move(self,turn):
+    if (self.turnCount == 0):
+      move = random.choice(self.avail_cols())
+      self.board = self.board.move(move,turn)
+      self.game = self.update()
+    else:
+      for i in range(2):
+        if self.game==False:
+          move = random.choice(self.avail_cols())
+          self.board = self.board.move(move,turn)
           self.game = self.update()
-          
-        if self.game == False:          
-          #check for filled columns that the computer cannot place a stone in
-          filledCols = []
-          for x,y in self.p1_positions + self.p2_positions:              
-              if y == self.board.height - 1:
-                  filledCols.append(x)
-          availableCols = [x for x in range(self.board.height) if x not in filledCols]         
-          #computer places a stone randomly in an available column
-          move = random.choice(availableCols)
-          self.board = self.board.move(move,2)
-          self.game = self.update()
-          
-        if self.game == False:              
-          #check for filled columns that the computer cannot place a stone in
-          for x,y in self.p1_positions + self.p2_positions:
-              if y == self.board.height - 1:
-                  if x not in filledCols:
-                      filledCols.append(x)
-          availableCols = [x for x in range(self.board.height) if x not in filledCols]  
-          #computer places a stone randomly in an available column
-          move = random.choice(availableCols)
-          self.board = self.board.move(move,2)
-          self.game = self.update()
-          #increment turn count
-          self.turnCount = self.turnCount + 1
-          
-    #if turn count is even, then player plays one stone
-    else:  
-        if self.game == False:
-          self.app.config(cursor="watch")
-          self.board = self.board.move(x,1)
-          self.game = self.update()   
-          #increment turn count
-          self.turnCount = self.turnCount + 1
-    self.app.config(cursor="")
+    self.turnCount = self.turnCount + 1
+
+  def agent_move(self, turn):
+    pass
+    #if (self.turnCount == 0):
+    #  move = get_agent_move()
+    #  self.board = self.board.move(move,turn)
+    #  self.game = self.update()
+    #else:
+    #  for i in range(2):
+    #    if self.game==False:
+    #      move = get_agent_move()
+    #      self.board = self.board.move(move,turn)
+    #      self.game = self.update()
+    #self.turnCount = self.turnCount + 1
+
+  def human_move(self,x,turn):
+    for i in x:
+      if self.game==False:
+        self.board = self.board.move(i,turn)
+        self.game = self.update()
+    self.turnCount = self.turnCount + 1
+
+  def input(self,x):
+    self.game = self.update()
+    if self.game==False:
+      self.inputx.append(x)
+      if self.turnCount==0:
+        self.move(self.inputx)
+        self.inputx = []
+      elif len(self.inputx)==2:
+        self.move(self.inputx)
+        self.inputx = []
+
+  def move(self,x=None):
+    if self.game==False:
+      if self.p1.name=="Human":
+        self.human_move(x,1)
+      elif self.p1.name=="Bot":
+        self.agent_move(1)
+      else:
+        self.random_move(1)
+    if self.game==False:
+      if self.p2.name=="Bot":
+        self.agent_move(2)
+      else:
+        self.random_move(2)
+
+  def autoplay(self):
+    if self.game==False:
+      if self.p1.name=="Bot":
+        self.agent_move(1)
+      else:
+        self.random_move(1)
+    if self.game==False:
+      if self.p2.name=="Bot":
+        self.agent_move(2)
+      else:
+        self.random_move(2)
 
   def update(self):
     for (x,y) in self.board.fields:
@@ -191,7 +241,7 @@ class GUI:
         if (x,y) not in self.p2_positions:
           self.p2_positions.append((x,y))
     for x in range(self.board.width):
-      if self.board.fields[x,self.board.height-1]==self.board.empty:
+      if self.board.fields[x,self.board.height-1]==self.board.empty and self.human_flag:
         self.buttons[x]['state'] = 'normal'
       else:
         self.buttons[x]['state'] = 'disabled'
@@ -212,7 +262,6 @@ class GUI:
       print "\nGame Over! Player",turn,"has won!"
       return True
     return False
-
 
 
   def mainloop(self):
